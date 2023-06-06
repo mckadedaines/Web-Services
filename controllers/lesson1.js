2774e37ce130c6d9662d6a14cbe3347c72b8965c
Mckade <mckadedaines1@gmail.com>
2023-06-06 15:39:52 -0700

    update

--- controllers/lesson1.js
+++ controllers/lesson1.js
@@ -6,7 +6,13 @@ const kennaRoute = (req, res) => {
     res.send('Kenna Daines');
 };
 
+const spencerRoute = (req, res) => {
+    res.send('Spencer Daines');
+};
+
+
 module.exports = {
     mckadeRoute,
-    kennaRoute
+    kennaRoute,
+    spencerRoute
 };
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const lesson1Controller = require('../controllers/lesson1');
 
 routes.get('/', lesson1Controller.mckadeRoute);
 routes.get('/kenna', lesson1Controller.kennaRoute);
+routes.get('/spencer', lesson1Controller.spencerRoute);
 
 module.exports = routes;
