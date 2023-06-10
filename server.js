f146c4834150d9f3df5f48d7a62160b83586a20a
Mckade <mckadedaines1@gmail.com>
2023-06-10 14:33:13 -0700

    update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const lesson2Controller = require('./Lesson2/Backend/controllers/contacts.js');
 
 const port = 8080;
 
-app.use('/', require('./Lesson2/Backend/routes/index.js'));
+app.use('/', require('./Lesson1/routes/index.js'));
 
 app.listen(process.env.PORT || 8080);
 console.log('Web Server is listening at port ' + (process.env.PORT || 8080));
