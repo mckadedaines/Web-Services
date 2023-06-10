30810a57b1b1321d9a176daccec3b9d4ac689004
Mckade <mckadedaines1@gmail.com>
2023-06-10 14:24:40 -0700

    update

--- Lesson2/Backend/routes/contacts.js
+++ Lesson2/Backend/routes/contacts.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const contactsController = require('../controllers/contacts');
+const contactsController = require('../controllers/contacts/id');
 
 router.get('/', contactsController.getAll);
 
